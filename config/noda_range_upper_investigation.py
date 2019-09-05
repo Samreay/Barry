2e55890c0001e900c222885231990a182ee445e2
Samuel Hinton <samuelreay@gmail.com>
2019-09-05 16:27:05 +1000

    Increasing number of tests for the final index

--- config/noda_range_upper_investigation.py
+++ config/noda_range_upper_investigation.py
@@ -19,20 +19,23 @@ if __name__ == "__main__":
     fitter = Fitter(dir_name)
 
     ps = [
-        BAOExtractor(r_s, extra_ks=(0.095, 0.10)),
-        BAOExtractor(r_s, extra_ks=(0.095, 0.12)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.13)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.14)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.15)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.16)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.17)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.18)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.19)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.20)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.21)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.22)),
+        BAOExtractor(r_s, extra_ks=(0.095, 0.23)),
         BAOExtractor(r_s, extra_ks=(0.095, 0.24)),
     ]
 
     recon = True
     for p in ps:
-        n = f"Anchor: $k = {p.extra_ks[1]} h/\\text{{Mpc}}$"
-        print(n)
+        n = f"$k = {p.extra_ks[1]:0.2f}\, h / {{\\rm Mpc}}$"
         model = PowerNoda2019(postprocess=p, recon=recon)
         data = PowerSpectrum_SDSS_DR12_Z061_NGC(min_k=0.02, max_k=0.30, postprocess=p, recon=recon)
         fitter.add_model_and_dataset(model, data, name=n)
@@ -40,7 +43,7 @@ if __name__ == "__main__":
     sampler = DynestySampler(temp_dir=dir_name)
 
     fitter.set_sampler(sampler)
-    fitter.set_num_walkers(10)
+    fitter.set_num_walkers(30)
     fitter.fit(file)
 
     if fitter.should_plot():
@@ -50,7 +53,7 @@ if __name__ == "__main__":
         for posterior, weight, chain, model, data, extra in fitter.load():
             print(extra["name"])
             c.add_chain(chain, weights=weight, parameters=model.get_labels(), **extra)
-        c.configure(shade=True, bins=30, legend_artists=True, cmap="plasma", )
+        c.configure(shade=True, bins=30, legend_artists=True, cmap="plasma", sigmas=[0, 1])
         extents = None # {"$\\alpha$": (0.88, 1.18), "$A$": (0, 10), "$b$": (1.5, 1.8), r"$\gamma_{rec}$": (1, 8)}
         params = ["$\\alpha$", "$A$", "$b$"]
         c.analysis.get_latex_table(filename=pfn + "_params.txt")
