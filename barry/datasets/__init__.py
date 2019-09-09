5088471912ab4f7f9c86d5b416494625142b7840
Samuel Hinton <samuelreay@gmail.com>
2019-09-09 11:20:38 +1000

    Updating models to remove duplication

--- barry/datasets/__init__.py
+++ barry/datasets/__init__.py
@@ -1,7 +1,13 @@
 from barry.datasets.dataset import Dataset, MultiDataset
 from barry.datasets.dataset_correlation_function import CorrelationFunction_SDSS_DR7_Z015_MGS, CorrelationFunction_SDSS_DR12_Z061_NGC
-from barry.datasets.dataset_power_spectrum import PowerSpectrum_SDSS_DR12_Z061_NGC, PowerSpectrum_SDSS_DR12_Z051_NGC, PowerSpectrum_SDSS_DR12_Z051_SGC, PowerSpectrum_SDSS_DR12_Z051, PowerSpectrum_SDSS_DR7_Z015
-from barry.datasets.dummy import DummyPowerSpectrum_SDSS_DR12_Z061_NGC
+from barry.datasets.dataset_power_spectrum import (
+    PowerSpectrum_SDSS_DR12_Z061_NGC,
+    PowerSpectrum_SDSS_DR12_Z051_NGC,
+    PowerSpectrum_SDSS_DR12_Z051_SGC,
+    PowerSpectrum_SDSS_DR12_Z051,
+    PowerSpectrum_SDSS_DR7_Z015,
+)
+from barry.datasets.dummy import DummyPowerSpectrum_SDSS_DR12_Z061_NGC, DummyCorrelationFunction_SDSS_DR12_Z061_NGC
 
 __all__ = [
     "PowerSpectrum_SDSS_DR12_Z061_NGC",
@@ -12,4 +18,5 @@ __all__ = [
     "CorrelationFunction_SDSS_DR7_Z015_MGS",
     "CorrelationFunction_SDSS_DR12_Z061_NGC",
     "DummyPowerSpectrum_SDSS_DR12_Z061_NGC",
+    "DummyCorrelationFunction_SDSS_DR12_Z061_NGC",
 ]
--- barry/datasets/dataset_power_spectrum.py
+++ barry/datasets/dataset_power_spectrum.py
@@ -5,29 +5,91 @@ from barry.datasets.dataset_power_spectrum_abc import PowerSpectrum
 
 class PowerSpectrum_SDSS_DR12_Z061_NGC(PowerSpectrum):
     def __init__(self, realisation=None, name=None, fake_diag=False, recon=True, min_k=0.02, max_k=0.3, reduce_cov_factor=1, step_size=1, postprocess=None):
-        super().__init__("sdss_dr12_ngc_pk_zbin0p61.pkl", name=name, min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
+        super().__init__(
+            "sdss_dr12_ngc_pk_zbin0p61.pkl",
+            name=name,
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
 
 
 class PowerSpectrum_SDSS_DR12_Z051_NGC(PowerSpectrum):
     def __init__(self, realisation=None, name=None, fake_diag=False, recon=True, min_k=0.02, max_k=0.3, reduce_cov_factor=1, step_size=1, postprocess=None):
-        super().__init__("sdss_dr12_ngc_pk_zbin0p51.pkl", name=name, min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
+        super().__init__(
+            "sdss_dr12_ngc_pk_zbin0p51.pkl",
+            name=name,
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
 
 
 class PowerSpectrum_SDSS_DR12_Z051_SGC(PowerSpectrum):
     def __init__(self, realisation=None, name=None, fake_diag=False, recon=True, min_k=0.02, max_k=0.3, reduce_cov_factor=1, step_size=1, postprocess=None):
-        super().__init__("sdss_dr12_sgc_pk_zbin0p51.pkl", name=name, min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
+        super().__init__(
+            "sdss_dr12_sgc_pk_zbin0p51.pkl",
+            name=name,
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
 
 
 class PowerSpectrum_SDSS_DR12_Z051(MultiDataset):
     def __init__(self, realisation=None, name=None, fake_diag=False, recon=True, min_k=0.02, max_k=0.3, reduce_cov_factor=1, step_size=1, postprocess=None):
-        ngc = PowerSpectrum_SDSS_DR12_Z051_NGC(min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
-        sgc = PowerSpectrum_SDSS_DR12_Z051_NGC(min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
+        ngc = PowerSpectrum_SDSS_DR12_Z051_NGC(
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
+        sgc = PowerSpectrum_SDSS_DR12_Z051_NGC(
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
         super().__init__(name, [ngc, sgc])
 
 
 class PowerSpectrum_SDSS_DR7_Z015(PowerSpectrum):
     def __init__(self, realisation=None, name=None, fake_diag=False, recon=True, min_k=0.02, max_k=0.3, reduce_cov_factor=1, step_size=5, postprocess=None):
-        super().__init__("sdss_dr7_pk.pkl", name=name, min_k=min_k, max_k=max_k, step_size=step_size, recon=recon, reduce_cov_factor=reduce_cov_factor, postprocess=postprocess, realisation=realisation, fake_diag=fake_diag)
+        super().__init__(
+            "sdss_dr7_pk.pkl",
+            name=name,
+            min_k=min_k,
+            max_k=max_k,
+            step_size=step_size,
+            recon=recon,
+            reduce_cov_factor=reduce_cov_factor,
+            postprocess=postprocess,
+            realisation=realisation,
+            fake_diag=fake_diag,
+        )
 
 
 if __name__ == "__main__":
