5efa159bf453f3b1199de71014da54e3c33d60cc
Samuel Hinton <samuelreay@gmail.com>
2019-10-21 12:20:43 +1000

    Updating consensus

--- investigations/get_consensus_measurement.py
+++ investigations/get_consensus_measurement.py
@@ -14,8 +14,11 @@ use_columns = [a for a in list(df_all.columns) if "Noda" not in a and "realisati
 
 df = df_all[use_columns]
 
-cols = [a for a in df.columns if "mean" in a]
-labels = [a.replace("_pk", " $P(k)$").replace("_mean", "").replace("_xi", " $\\xi(s)$").replace("Recon", "") for a in cols]
+cols = [a for a in df.columns if "mean" in a and "Beutler 2017 R" not in a and "Beutler 2017 P" not in a]
+labels = [a for a in cols]
+swaps = [("_pk", " $P(k)$"), ("_mean", ""), ("_xi", " $\\xi(s)$"), ("Recon", ""), ("Beutler 2017", "B17"), ("Ding 2018", "D18"), ("Seo 2016", "S16")]
+for a, b in swaps:
+    labels = [l.replace(a, b) for l in labels]
 means = df[cols].values
 stds = df[[a for a in df.columns if "std" in a]].values
 
@@ -42,4 +45,9 @@ print("Mean scatter: ", np.sqrt(np.diag(cov)))
 print("Consensus err: ", combined_err)
 print("ratios: ", 1.0 / np.sqrt(sigma_c * np.diag(cov)))
 
-# Answer: Yes, by between 5-10%
+# This makes no sense - how is the consensus measurement more biased than anything going into it??
+# Mean measurements:  [1.00039962 0.99850631 0.99840441 1.00052836 1.00052664 1.00028977]
+# Consensus:  1.0025964898749093
+# Mean scatter:  [0.01222109 0.01262812 0.01268712 0.0133334  0.01333228 0.01336515]
+# Consensus err:  0.012027931643257472
+# ratios:  [0.98419472 0.952472   0.94804269 0.90209063 0.90216599 0.89994762]
