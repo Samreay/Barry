2ce8bb2ce2362230514475b0ddce32b96dc36489
Cullan Howlett <cullan.howlett@gmail.com>
2019-10-22 13:55:40 +0800

    Added more plots to xi_avg and pk_avg

--- config/pk_avg.py
+++ config/pk_avg.py
@@ -2,7 +2,7 @@ import sys
 
 sys.path.append("..")
 from barry.cosmology.camb_generator import getCambGenerator
-from barry.postprocessing import BAOExtractor
+from barry.postprocessing import BAOExtractor, PureBAOExtractor
 from barry.config import setup
 from barry.models import PowerSeo2016, PowerBeutler2017, PowerDing2018, PowerNoda2019
 from barry.datasets import PowerSpectrum_SDSS_DR12_Z061_NGC
@@ -47,21 +47,72 @@ if __name__ == "__main__":
         import logging
 
         logging.info("Creating plots")
-        from chainconsumer import ChainConsumer
 
-        c = ChainConsumer()
-        for posterior, weight, chain, evidence, model, data, extra in fitter.load():
-            c.add_chain(chain, weights=weight, parameters=model.get_labels(), **extra)
-        c.configure(shade=True, bins=30, legend_artists=True)
-        c.analysis.get_latex_table(filename=pfn + "_params.txt", parameters=[r"$\alpha$"])
-        c.plotter.plot_summary(
-            filename=[pfn + "_summary2.png", pfn + "_summary2.pdf"],
-            extra_parameter_spacing=1.5,
-            parameters=1,
-            errorbar=True,
-            truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0},
-        )
-        c.plotter.plot_summary(filename=[pfn + "_summary.png", pfn + "_summary.pdf"], errorbar=True, truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
-        c.plotter.plot(filename=[pfn + "_contour.png", pfn + "_contour.pdf"], truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0}, parameters=2)
-        c.plotter.plot(filename=[pfn + "_contour2.png", pfn + "_contour2.pdf"], truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
-        # c.plotter.plot_walks(filename=pfn + "_walks.png", truth={"$\\Omega_m$": 0.3121, '$\\alpha$': 1.0})
+        if False:
+            from chainconsumer import ChainConsumer
+
+            c = ChainConsumer()
+            for posterior, weight, chain, model, data, extra in fitter.load():
+                c.add_chain(chain, weights=weight, parameters=model.get_labels(), **extra)
+            c.configure(shade=True, bins=30, legend_artists=True)
+            c.analysis.get_latex_table(filename=pfn + "_params.txt", parameters=[r"$\alpha$"])
+            c.plotter.plot_summary(
+                filename=[pfn + "_summary2.png", pfn + "_summary2.pdf"],
+                extra_parameter_spacing=1.5,
+                parameters=1,
+                errorbar=True,
+                truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0},
+            )
+            c.plotter.plot_summary(filename=[pfn + "_summary.png", pfn + "_summary.pdf"], errorbar=True, truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
+            c.plotter.plot(filename=[pfn + "_contour.png", pfn + "_contour.pdf"], truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0}, parameters=2)
+            c.plotter.plot(filename=[pfn + "_contour2.png", pfn + "_contour2.pdf"], truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
+            # c.plotter.plot_walks(filename=pfn + "_walks.png", truth={"$\\Omega_m$": 0.3121, '$\\alpha$': 1.0})
+
+        # Plots the average recon mock measurements and the best-fit models from each of the models tested.
+        # We'll also plot the ratio for everything against the smooth Beutler2017 model.
+        if True:
+
+            import numpy as np
+            import matplotlib.pyplot as plt
+            import matplotlib.gridspec as gridspec
+
+            fig, axes = plt.subplots(figsize=(6, 8), nrows=2, sharex=True, gridspec_kw={"hspace": 0.06, "height_ratios": [4.3, 1]})
+            inner = gridspec.GridSpecFromSubplotSpec(4, 1, subplot_spec=axes[0], hspace=0.04)
+            ax = plt.subplot(inner[0:])
+            ax.spines["top"].set_color("none")
+            ax.spines["bottom"].set_color("none")
+            ax.spines["left"].set_color("none")
+            ax.spines["right"].set_color("none")
+            ax.set_ylabel(r"$P(k) / P_{\mathrm{smooth}}(k)$")
+            ax.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
+
+            counter = 0
+            for posterior, weight, chain, model, data, extra in fitter.load():
+                if not "Recon" in extra["name"]:
+                    continue
+                if extra["name"] == "Noda 2019 Recon":
+                    model.postprocess = PureBAOExtractor(r_s)
+                model.set_data(data)
+                p = model.get_param_dict(chain[np.argmax(posterior)])
+                smooth = model.get_model(p, data[0], smooth=True)
+                bestfit = model.get_model(p, data[0])
+                if counter == 0:
+                    ks = data[0]["ks"]
+                    pk = data[0]["pk"]
+                    err = np.sqrt(np.diag(data[0]["cov"]))
+                    rk = PowerSpectrum_SDSS_DR12_Z061_NGC(recon=True, postprocess=PureBAOExtractor(r_s)).get_data()[0]
+                if extra["name"] == "Noda 2019 Recon":
+                    axes[1].errorbar(ks, rk["pk"], yerr=np.sqrt(np.diag(rk["cov"])), fmt="o", c="k", ms=4, zorder=0)
+                    axes[1].plot(ks, bestfit, color=extra["color"], label=extra["name"], linestyle=extra["linestyle"], zorder=1, linewidth=1.8)
+                    axes[1].set_ylabel(r"$R(k)$", labelpad=-5)
+                    axes[1].legend()
+                else:
+                    ax = fig.add_subplot(inner[counter])
+                    ax.errorbar(ks, pk / smooth, yerr=err / smooth, fmt="o", c="k", ms=4, zorder=0)
+                    ax.plot(ks, bestfit / smooth, color=extra["color"], label=extra["name"], linestyle=extra["linestyle"], zorder=1, linewidth=1.8)
+                    ax.set_ylim(0.88, 1.12)
+                    ax.tick_params(axis="x", which="both", labelcolor="none", bottom=False, labelbottom=False)
+                    ax.legend()
+                counter += 1
+            axes[1].set_xlabel(r"$k\,(h\,\mathrm{Mpc^{-1}})$")
+            plt.savefig(pfn + "_bestfits.pdf")
--- config/xi_avg.py
+++ config/xi_avg.py
@@ -44,21 +44,64 @@ if __name__ == "__main__":
         import logging
 
         logging.info("Creating plots")
-        from chainconsumer import ChainConsumer
 
-        c = ChainConsumer()
-        for posterior, weight, chain, evidence, model, data, extra in fitter.load():
-            c.add_chain(chain, weights=weight, parameters=model.get_labels(), **extra)
-            print(extra["name"], chain.shape, weight.shape, posterior.shape)
-        c.configure(shade=True, bins=30, legend_artists=True)
-        c.analysis.get_latex_table(filename=pfn + "_params.txt", parameters=[r"$\alpha$"])
-        c.plotter.plot_summary(filename=pfn + "_summary.png", extra_parameter_spacing=1.5, errorbar=True, truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
-        c.plotter.plot_summary(
-            filename=[pfn + "_summary2.png", pfn + "_summary2.pdf"],
-            extra_parameter_spacing=1.5,
-            parameters=1,
-            errorbar=True,
-            truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0},
-        )
-        # c.plotter.plot(filename=pfn + "_contour.png", truth={"$\\Omega_m$": 0.31, '$\\alpha$': 1.0})
-        # c.plotter.plot_walks(filename=pfn + "_walks.png", truth={"$\\Omega_m$": 0.3121, '$\\alpha$': 1.0})
+        if False:
+            from chainconsumer import ChainConsumer
+
+            c = ChainConsumer()
+            for posterior, weight, chain, model, data, extra in fitter.load():
+                c.add_chain(chain, weights=weight, parameters=model.get_labels(), **extra)
+                print(extra["name"], chain.shape, weight.shape, posterior.shape)
+            c.configure(shade=True, bins=30, legend_artists=True)
+            c.analysis.get_latex_table(filename=pfn + "_params.txt", parameters=[r"$\alpha$"])
+            c.plotter.plot_summary(filename=pfn + "_summary.png", extra_parameter_spacing=1.5, errorbar=True, truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0})
+            c.plotter.plot_summary(
+                filename=[pfn + "_summary2.png", pfn + "_summary2.pdf"],
+                extra_parameter_spacing=1.5,
+                parameters=1,
+                errorbar=True,
+                truth={"$\\Omega_m$": 0.31, "$\\alpha$": 1.0},
+            )
+            # c.plotter.plot(filename=pfn + "_contour.png", truth={"$\\Omega_m$": 0.31, '$\\alpha$': 1.0})
+            # c.plotter.plot_walks(filename=pfn + "_walks.png", truth={"$\\Omega_m$": 0.3121, '$\\alpha$': 1.0})
+
+        # Plots the average recon mock measurements and the best-fit models from each of the models tested.
+        # We'll also plot the ratio for everything against the smooth Beutler2017 model.
+        if True:
+
+            import numpy as np
+            import matplotlib.pyplot as plt
+            import matplotlib.gridspec as gridspec
+
+            fig, axes = plt.subplots(figsize=(6, 8), nrows=1, sharex=True, gridspec_kw={"hspace": 0.06, "height_ratios": [4.3, 1]})
+            inner = gridspec.GridSpecFromSubplotSpec(4, 1, subplot_spec=axes[0], hspace=0.04)
+            ax = plt.subplot(inner[0:])
+            ax.spines["top"].set_color("none")
+            ax.spines["bottom"].set_color("none")
+            ax.spines["left"].set_color("none")
+            ax.spines["right"].set_color("none")
+            ax.set_ylabel(r"$\xi(s) - \xi_{\mathrm{smooth}}(s)$")
+            ax.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
+
+            counter = 0
+            for posterior, weight, chain, model, data, extra in fitter.load():
+                if not "Recon" in extra["name"]:
+                    continue
+                model.set_data(data)
+                print(data[0].keys())
+                p = model.get_param_dict(chain[np.argmax(posterior)])
+                smooth = model.get_model(p, data[0], smooth=True)
+                bestfit = model.get_model(p, data[0])
+                if counter == 0:
+                    ss = data[0]["dist"]
+                    xi = data[0]["xi0"]
+                    err = np.sqrt(np.diag(data[0]["cov"]))
+                    ax = fig.add_subplot(inner[counter])
+                    ax.errorbar(ss, xi - smooth, yerr=err / smooth, fmt="o", c="k", ms=4, zorder=0)
+                    ax.plot(ss, bestfit - smooth, color=extra["color"], label=extra["name"], linestyle=extra["linestyle"], zorder=1, linewidth=1.8)
+                    ax.set_ylim(0.88, 1.12)
+                    ax.tick_params(axis="x", which="both", labelcolor="none", bottom=False, labelbottom=False)
+                    ax.legend()
+                counter += 1
+            axes[1].set_xlabel(r"$s\,(h^{-1}\,\mathrm{Mpc})$")
+            plt.savefig(pfn + "_bestfits.pdf")
