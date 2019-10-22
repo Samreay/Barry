a313a30c737e27f1baa243a4a68f39176a311237
Samuel Hinton <samuelreay@gmail.com>
2019-10-22 20:28:39 +1000

    Bayesian model averaging. Not very useful for our context

--- investigations/get_consensus_measurement.py
+++ investigations/get_consensus_measurement.py
@@ -11,7 +11,7 @@ df_pk = pd.read_csv(filename_pk)
 df_xi = pd.read_csv(filename_xi)
 
 df_all = pd.merge(df_pk, df_xi, on="realisation")
-use_columns = [a for a in list(df_all.columns) if "Noda" not in a and "realisation" not in a and "Recon" in a]
+use_columns = [a for a in list(df_all.columns) if "Noda" not in a and "realisation" not in a and "Recon" in a and "xi" in a]
 
 df = df_all[use_columns]
 
