0b743b7dd607ec6aa28dff0511ca8a19f221cfe4
Samuel Hinton <samuelreay@gmail.com>
2019-12-10 09:13:19 -0500

    Adding Ross dr12 correlation functions

--- barry/datasets/__init__.py
+++ barry/datasets/__init__.py
@@ -27,13 +27,19 @@ Concrete
    PowerSpectrum_SDSS_DR7_Z015
    DummyCorrelationFunction_SDSS_DR12_Z061_NGC
    DummyPowerSpectrum_SDSS_DR12_Z061_NGC
-   CorrelationFunction_SDSS_DR7_Z015_MGS
    CorrelationFunction_SDSS_DR12_Z061_NGC
-
+   CorrelationFunction_ROSS_DR12_Z038
+   CorrelationFunction_ROSS_DR12_Z051
+   CorrelationFunction_ROSS_DR12_Z061
 """
 
 from barry.datasets.dataset import Dataset, MultiDataset
-from barry.datasets.dataset_correlation_function import CorrelationFunction_SDSS_DR12_Z061_NGC
+from barry.datasets.dataset_correlation_function import (
+    CorrelationFunction_SDSS_DR12_Z061_NGC,
+    CorrelationFunction_ROSS_DR12_Z038,
+    CorrelationFunction_ROSS_DR12_Z051,
+    CorrelationFunction_ROSS_DR12_Z061,
+)
 from barry.datasets.dataset_power_spectrum import (
     PowerSpectrum_SDSS_DR12_Z061_NGC,
     PowerSpectrum_SDSS_DR12_Z051_NGC,
@@ -48,6 +54,9 @@ __all__ = [
     "PowerSpectrum_SDSS_DR12_Z051_SGC",
     "PowerSpectrum_SDSS_DR12_Z051",
     "CorrelationFunction_SDSS_DR12_Z061_NGC",
+    "CorrelationFunction_ROSS_DR12_Z038",
+    "CorrelationFunction_ROSS_DR12_Z051",
+    "CorrelationFunction_ROSS_DR12_Z061",
     "DummyPowerSpectrum_SDSS_DR12_Z061_NGC",
     "DummyCorrelationFunction_SDSS_DR12_Z061_NGC",
 ]
