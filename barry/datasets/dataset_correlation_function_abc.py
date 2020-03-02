Fixing issue in setting covariance and generalising pk2 and pk4

barry/datasets/dataset_correlation_function_abc.py
@@ -51,22 +51,25 @@ class CorrelationFunction(Dataset, ABC):
         self.mask = (self.data[:, 0] >= self.min_dist) & (self.data[:, 0] <= self.max_dist)
         self.data = self.data[self.mask, :]
 
-    def set_cov(self):
-        covname = "post-recon cov" if self.recon else "pre-recon cov"
-        if covname in self.data_obj:
-            npoles = 1 if self.isotropic else 2
-            nin = len(self.mask)
-            nout = self.data.shape[0]
-            self.cov = np.empty((npoles * nout, npoles * nout))
-            for i in range(npoles):
-                iinlow, iinhigh = i * nin, (i + 1) * nin
-                ioutlow, iouthigh = i * nout, (i + 1) * nout
-                for j in range(npoles):
-                    jinlow, jinhigh = j * nin, (j + 1) * nin
-                    joutlow, jouthigh = j * nout, (j + 1) * nout
-                    self.cov[ioutlow:iouthigh, joutlow:jouthigh] = self.data_obj[covname][iinlow:iinhigh, jinlow:jinhigh][np.ix_(self.mask, self.mask)]
+    def set_cov(self, cov=None):
+        if cov is None:
+            covname = "post-recon cov" if self.recon else "pre-recon cov"
+            if covname in self.data_obj:
+                npoles = 1 if self.isotropic else 2
+                nin = len(self.mask)
+                nout = self.data.shape[0]
+                self.cov = np.empty((npoles * nout, npoles * nout))
+                for i in range(npoles):
+                    iinlow, iinhigh = i * nin, (i + 1) * nin
+                    ioutlow, iouthigh = i * nout, (i + 1) * nout
+                    for j in range(npoles):
+                        jinlow, jinhigh = j * nin, (j + 1) * nin
+                        joutlow, jouthigh = j * nout, (j + 1) * nout
+                        self.cov[ioutlow:iouthigh, joutlow:jouthigh] = self.data_obj[covname][iinlow:iinhigh, jinlow:jinhigh][np.ix_(self.mask, self.mask)]
+            else:
+                self._compute_cov()
         else:
-            self._compute_cov()
+            self.cov = cov
         self.cov /= self.reduce_cov_factor
         self.icov = np.linalg.inv(self.cov)
 