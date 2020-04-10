9d432f75c0414e0d7b6d646799fd872b5a1455e9
Cullan Howlett <cullan.howlett@gmail.com>
2020-04-10 15:53:55 +1000

    Add more desi mock challenge

--- barry/data/desi_mock_challenge_post_recon/pickle_handshake.py
+++ barry/data/desi_mock_challenge_post_recon/pickle_handshake.py
@@ -35,7 +35,7 @@ def getcomp(ks):
 if __name__ == "__main__":
 
     ds = f"/global/project/projectdirs/desi/users/UNIT-BAO-RSD-challenge/EZmocks/pk_EZmocks/"
-    files = [ds + f for f in ds]
+    files = [ds + f for k, f in os.listdir(ds) if "Power_Spectrum" in k]
     print(files)
 
     pk_filename = f"/global/project/projectdirs/desi/users/UNIT-BAO-RSD-challenge/UNIT/pk/Power_Spectrum_UNIT_HODsnap97_ELGv1_redshift.txt"
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     data = getdf(pk_filename)
     ks = data["k"].to_numpy()
     cov = pd.read_csv(cov_filename, delim_whitespace=True, header=None).to_numpy()
-    cov = (cov.astype(np.float32)[:, 2]).reshape((len(ks), len(ks)))
+    cov = (cov.astype(np.float32)[:, 2]).reshape((3 * len(ks), 3 * len(ks)))
 
     res = {f.lower(): getdf(f) for f in files}
     split = {
