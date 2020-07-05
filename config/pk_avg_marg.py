e9c6d7663a83d52741aa5a079f532974543f499d
Cullan Howlett <cullan.howlett@gmail.com>
2020-07-05 19:57:02 +1000

    Adding new analytic marginalisation

--- config/pk_avg_marg.py
+++ config/pk_avg_marg.py
@@ -49,10 +49,22 @@ if __name__ == "__main__":
         model_fixed.set_default(f"a{{0}}_3", 2137.0)
         model_fixed.set_default(f"a{{0}}_4", -25.43)
         model_fixed.set_default(f"a{{0}}_5", 0.01628)
+        model_fixed_poly = PowerBeutler2017(
+            recon=r,
+            isotropic=d.isotropic,
+            fix_params=["om", f"a{{0}}_1", f"a{{0}}_2", f"a{{0}}_3", f"a{{0}}_4", f"a{{0}}_5"],
+            correction=Correction.HARTLAP,
+        )
+        model_fixed_poly.set_default(f"a{{0}}_1", 4651.0)
+        model_fixed_poly.set_default(f"a{{0}}_2", -4882.0)
+        model_fixed_poly.set_default(f"a{{0}}_3", 2137.0)
+        model_fixed_poly.set_default(f"a{{0}}_4", -25.43)
+        model_fixed_poly.set_default(f"a{{0}}_5", 0.01628)
 
-        fitter.add_model_and_dataset(model, d, name=f"Full Fit", linestyle=ls, color=cs[1])
-        fitter.add_model_and_dataset(model_marg, d, name=f"Marginalised", linestyle=ls, color=cs[2])
-        fitter.add_model_and_dataset(model_fixed, d, name=f"Fixed Nuisance", linestyle=ls, color=cs[3])
+        # fitter.add_model_and_dataset(model, d, name=f"Full Fit", linestyle=ls, color=cs[1])
+        # fitter.add_model_and_dataset(model_marg, d, name=f"Analytic", linestyle=ls, color=cs[2])
+        # fitter.add_model_and_dataset(model_fixed, d, name=f"Fixed Bias+Poly", linestyle=ls, color=cs[3])
+        fitter.add_model_and_dataset(model_fixed_poly, d, name=f"Fixed Poly", linestyle=ls, color=cs[3])
         # fitter.add_model_and_dataset(model, d, name=f"Beutler 2017 Fixed $\\Sigma_{{nl}}$ {t}", linestyle=ls, color=cs[0])
         # fitter.add_model_and_dataset(PowerSeo2016(recon=r, isotropic=False), d, name=f"Seo 2016 {t}", linestyle=ls, color=cs[1])
         # fitter.add_model_and_dataset(PowerDing2018(recon=r, isotropic=False), d, name=f"Ding 2018 {t}", linestyle=ls, color=cs[2])
