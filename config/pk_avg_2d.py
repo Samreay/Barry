012e3858d2b97c34c7e0dc114981f7181f8e3eb3
Cullan Howlett <cullan.howlett@gmail.com>
2020-07-05 20:01:52 +1000

    Adding new analytic marginalisation

--- config/pk_avg_2d.py
+++ config/pk_avg_2d.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         # Fix sigma_nl for one of the Beutler models
         model = PowerBeutler2017(recon=r, isotropic=False, fix_params=["om"], correction=Correction.HARTLAP)
         model_marg = PowerBeutler2017(recon=r, isotropic=False, fix_params=["om"], correction=Correction.HARTLAP, marg=True)
-        """model_fixed = PowerBeutler2017(
+        model_fixed = PowerBeutler2017(
             recon=r,
             isotropic=False,
             fix_params=[
@@ -66,11 +66,40 @@ if __name__ == "__main__":
         model_fixed.set_default(f"a{{2}}_2", 2551)
         model_fixed.set_default(f"a{{2}}_3", -282.9)
         model_fixed.set_default(f"a{{2}}_4", 15.86)
-        model_fixed.set_default(f"a{{2}}_5", -0.01816)"""
+        model_fixed.set_default(f"a{{2}}_5", -0.01816)
+        model_fixed_poly = PowerBeutler2017(
+            recon=r,
+            isotropic=False,
+            fix_params=[
+                "om",
+                f"a{{0}}_1",
+                f"a{{0}}_2",
+                f"a{{0}}_3",
+                f"a{{0}}_4",
+                f"a{{0}}_5",
+                f"a{{2}}_1",
+                f"a{{2}}_2",
+                f"a{{2}}_3",
+                f"a{{2}}_4",
+                f"a{{2}}_5",
+            ],
+            correction=Correction.HARTLAP,
+        )
+        model_fixed_poly.set_default(f"a{{0}}_1", -6427)
+        model_fixed_poly.set_default(f"a{{0}}_2", 4593)
+        model_fixed_poly.set_default(f"a{{0}}_3", -599.1)
+        model_fixed_poly.set_default(f"a{{0}}_4", 18.19)
+        model_fixed_poly.set_default(f"a{{0}}_5", -0.02162)
+        model_fixed_poly.set_default(f"a{{2}}_1", -5420)
+        model_fixed_poly.set_default(f"a{{2}}_2", 2551)
+        model_fixed_poly.set_default(f"a{{2}}_3", -282.9)
+        model_fixed_poly.set_default(f"a{{2}}_4", 15.86)
+        model_fixed_poly.set_default(f"a{{2}}_5", -0.01816)
 
         fitter.add_model_and_dataset(model, d, name=f"Full Fit", linestyle=ls, color=cs[1])
-        fitter.add_model_and_dataset(model_marg, d, name=f"Marginalised", linestyle=ls, color=cs[2])
-        # fitter.add_model_and_dataset(model_fixed, d, name=f"Fixed Nuisance", linestyle=ls, color=cs[3])
+        fitter.add_model_and_dataset(model_marg, d, name=f"Analytic", linestyle=ls, color=cs[2])
+        fitter.add_model_and_dataset(model_fixed, d, name=f"Fixed Bias+Poly", linestyle=ls, color=cs[3])
+        fitter.add_model_and_dataset(model_fixed_poly, d, name=f"Fixed Poly", linestyle=ls, color=cs[3])
         # fitter.add_model_and_dataset(model, d, name=f"Beutler 2017 Fixed $\\Sigma_{{nl}}$ {t}", linestyle=ls, color=cs[0])
         # fitter.add_model_and_dataset(PowerSeo2016(recon=r, isotropic=False), d, name=f"Seo 2016 {t}", linestyle=ls, color=cs[1])
         # fitter.add_model_and_dataset(PowerDing2018(recon=r, isotropic=False), d, name=f"Ding 2018 {t}", linestyle=ls, color=cs[2])
