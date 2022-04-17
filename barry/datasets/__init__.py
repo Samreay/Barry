0233cff12a9340f4620ba4886adb79833c682fcd
Cullan Howlett <cullan.howlett@gmail.com>
2022-04-17 20:48:48 +1000

    Adding in KP4 correlation function class

--- barry/datasets/__init__.py
+++ barry/datasets/__init__.py
@@ -29,6 +29,7 @@ from barry.datasets.dataset import Dataset, MultiDataset
 from barry.datasets.dataset_correlation_function import (
     # CorrelationFunction_SDSS_DR12_Z061_NGC,
     CorrelationFunction_ROSS_DR12,
+    CorrelationFunction_DESI_KP4,
 )
 from barry.datasets.dataset_power_spectrum import (
     PowerSpectrum_SDSS_DR12,
