ecd1ca136d51f4e8168e278c2f7a27ff9b8b17d3
Abbe May Whitford <abbe.whitford@gmail.com>
2023-07-18 23:41:31 -0700

    changes are from testing only

--- desi_kp4_abacus_cubic_ELG.py
+++ desi_kp4_abacus_cubic_ELG.py
@@ -18,9 +18,13 @@ if __name__ == "__main__":
     # Get the relative file paths and names
     pfn, dir_name, file = setup(__file__)
     
+    #print(file)
+    
+    #exit()
+    
     # Set up the Fitting class and Dynesty sampler with 500 live points. 
     # Set remove_output=False to make sure that we don't delete/overwrite existing chains in the same directory.
-    fitter = Fitter(dir_name, remove_output=False)
+    fitter = Fitter(dir_name)#, remove_output=False) 
     sampler = DynestySampler(temp_dir=dir_name, nlive=500)
     
     # The optimal sigma values we found when fitting the mocks with fixed alpha/epsilon
@@ -43,7 +47,7 @@ if __name__ == "__main__":
                 fit_poles=[0, 2],
                 min_k=0.02,
                 max_k=0.30,
-                realisation=None,          # realisation=None while load the average of all the realisations
+                realisation=None,          # realisation=None loads the average of all the realisations
                 num_mocks=1000,            # Used for Hartlap/Sellentin correction if correction=Correction.HARTLAP or Correction.SELLENTIN
                 reduce_cov_factor=1,       # Use standard covariance, even for the average
                 datafile=mockname+"_pk_elg.pkl",
@@ -69,11 +73,34 @@ if __name__ == "__main__":
                 recon=dataset_pk.recon,                   
                 isotropic=dataset_pk.isotropic,
                 marg="full",                              # Analytic marginalisation
+                #fix_params=[],#["om"],
                 poly_poles=dataset_pk.fit_poles,
                 correction=Correction.NONE,               # No covariance matrix debiasing
                 n_poly=6,                                 # 6 polynomial terms for P(k)
+                #vary_neff=True, 
             )
             
+#             print(dataset_pk.recon)
+#             print(dataset_pk.isotropic)
+#             print(dataset_pk.fit_poles)
+            
+#             #exit()
+#             cosmology = {
+#             "om": 0.31,
+#             "h0": 0.67,
+#             "z":  0.51,
+#             "ob": 0.048,
+#             "ns": 0.97,
+#             "mnu": 0.0,
+#             "reconsmoothscale": 15,
+#             "Neff": 3.044,
+#         }
+#             model.set_cosmology(cosmology)
+            
+#             params_dict = {i.name: i.default for i in model.params}
+#             res = model.compute_power_spectrum(np.logspace(-2, 2, 100, base=10), params_dict)
+            
+#             exit()
             # Set Gaussian priors for the BAO damping centred on the optimal values 
             # found from fitting with fixed alpha/epsilon and with width 2 Mpc/h
             model.set_default("sigma_nl_par", sigma_nl_par[recon], min=0.0, max=20.0, sigma=2.0, prior="gaussian")
@@ -128,7 +155,7 @@ if __name__ == "__main__":
                 fitter.add_model_and_dataset(model, dataset_xi, name=name)
                 allnames.append(name)
 
-    print(allnames)
+    #print(allnames)
                 
     # Set the sampler (dynesty) and assign 1 walker (processor) to each. If we assign more than one walker, for dynesty
     # this means running independent chains which will then get added together when they are loaded in.
